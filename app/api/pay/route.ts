0de17cf48d60df4d43e41b7e854722c2fd5e7b19
Bryam Loaiza <bryamloaiza@Bryams-MacBook-Pro-2.local>
2024-01-06 19:28:47 -0500

    wSS

--- app/api/pay/route.ts
+++ app/api/pay/route.ts
@@ -1,21 +1,40 @@
-import Stripe from "stripe";
-import { NextResponse, NextRequest } from "next/server";
+// Import necessary dependencies
+import { NextRequest, NextResponse } from 'next/server';
+import { Stripe } from 'stripe';
+import {headers} from "next/headers";
 
-export async function POST (request: NextRequest) {
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "");
-    let data = await request.json();
-    let priceId = data.priceId
+// Create a new instance of the Stripe client
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+  apiVersion: '2023-10-16',
+});
+
+// Define your API route handler using the POST function
+export async function POST(req: NextRequest, res: NextResponse<string>) {
+  try {
+    const { userId, email, purchaseType } = await req.json();
+
     const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                price: priceId,
-                quantity: 1
-            }
-        ],
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: 'price_1OVcBaLxbwyf0mciZyDDfkgD', 
+          quantity: 1,
+        },
+      ],
+      customer_email: email,
+      metadata: {
+        userId: userId,
+        purchaseType: purchaseType,
+      },
       mode: 'payment',
       success_url: 'http://localhost:3000',
-      cancel_url: 'http://localhost:3000'
-    })
+      cancel_url: 'http://localhost:3000',
+      client_reference_id: userId,
+    });
 
     return NextResponse.json(session.url)
+  } catch (err) {
+ 
+  }
+  
 }
