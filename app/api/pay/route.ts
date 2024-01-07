fd960096a08f36b07e24d974884daabc62cda399
Bryam Loaiza <bryamloaiza@Bryams-MacBook-Pro-2.local>
2024-01-06 20:04:59 -0500

    web

--- app/api/pay/route.ts
+++ app/api/pay/route.ts
@@ -17,8 +17,8 @@ export async function POST(req: NextRequest, res: NextResponse<string>) {
       payment_method_types: ['card'],
       line_items: [
         {
-          //price: 'price_1OVcBaLxbwyf0mciZyDDfkgD',
-          price: 'price_1OSPdmLxbwyf0mciDtKjHAFN',
+          price: 'price_1OVcBaLxbwyf0mciZyDDfkgD',
+          //price: 'price_1OSPdmLxbwyf0mciDtKjHAFN',
           quantity: 1,
         },
       ],
