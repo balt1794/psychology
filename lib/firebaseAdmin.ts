5fbf14c613739636b07cea1234b2a38f14083722
Bryam <bal22@njit.edu>
2026-06-19 23:10:51 -0400

    psychology 3s

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,11 +1,11 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
-import serviceAccount from "@/plai-c72e4-firebase-adminsdk-xffo1-054dd32a73.json";
+//import serviceAccount from "@/plai-c72e4-firebase-adminsdk-xffo1-054dd32a73.json";
 
 const firebaseAdminApp =
   getApps()[0] ||
   initializeApp({
-    credential: cert(serviceAccount as any),
+    //credential: cert(serviceAccount as any),
     storageBucket: "plai-c72e4.firebasestorage.app",
   });
 
